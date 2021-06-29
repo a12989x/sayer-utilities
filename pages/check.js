beabe52cf6aaa405d6886b353d99e53fcafad658
Alexis Guzman <alexis12989x@gmail.com>
2021-06-29 17:43:04 -0500

    feat(page): add info to how to use tool and add styles

--- pages/check.js
+++ pages/check.js
@@ -2,9 +2,26 @@ import Check from '@/components/Check';
 
 const check = () => {
 	return (
-		<div>
+		<main>
+			<h1>Conocer ganancia</h1>
+			<p>Conoce la cantidad y el porcentaje que ganas de un producto.</p>
+
+			<h2>Como Funciona:</h2>
+			<p>
+				Se deberán de ingresar dos valores, uno siendo el costo inicial
+				del producto (<label htmlFor='cost'>Costo</label>) y el segundo
+				valor siendo el precio final (
+				<label htmlFor='sale'>Venta</label>).
+			</p>
+			<p>
+				A la derecha se mostrarán los resultados siendo valor el de
+				arriba (<label htmlFor='currency'>Pesos</label>) el margen de
+				ganancia en pesos y el de abajo (
+				<label htmlFor='percentage'>Porcentaje</label>) el margen de
+				ganancia representado en porcentaje.
+			</p>
 			<Check />
-		</div>
+		</main>
 	);
 };
 
