7ab643a6a69b1e571313fe4c4f74ce8bd3d11ea7
Alexis Guzman <alexis12989x@gmail.com>
2021-09-06 15:00:08 -0500

    feat(seo): import head and default seo components

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,24 @@
 import { ThemeProvider } from 'next-themes';
+import { DefaultSeo } from 'next-seo';
+
+import SEO from '../next-seo.config.js';
 
 import Layout from '@/components/layout';
+import Head from '@/components/Head';
 
 import '@/styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }) => {
 	return (
-		<ThemeProvider defaultTheme='system'>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
-		</ThemeProvider>
+		<>
+			<Head />
+			<DefaultSeo {...SEO} />
+			<ThemeProvider defaultTheme='system'>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</ThemeProvider>
+		</>
 	);
 };
 
