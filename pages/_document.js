5d56c516464648950bc23278cc7aa609f492c2c4
Alexis Guzman <alexis12989x@gmail.com>
2021-09-06 14:59:29 -0500

    feat(seo): add meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -22,7 +22,38 @@ class MyDocument extends Document {
 	render() {
 		return (
 			<Html lang='es'>
-				<Head />
+				<Head>
+					<link
+						rel='apple-touch-icon'
+						sizes='180x180'
+						href='/favicons/apple-touch-icon.png'
+					/>
+					<link
+						rel='icon'
+						type='image/png'
+						sizes='32x32'
+						href='/favicons/favicon-32x32.png'
+					/>
+					<link
+						rel='icon'
+						type='image/png'
+						sizes='16x16'
+						href='/favicons/favicon-16x16.png'
+					/>
+					<link
+						rel='mask-icon'
+						href='/favicons/safari-pinned-tab.svg'
+						color='#1a8ccb'
+					/>
+					<meta
+						name='msapplication-TileImage'
+						content='/favicons/mstile-144x144.png'
+					/>
+					<meta name='msapplication-TileColor' content='#ffffff' />
+					<meta name='theme-color' content='#ffffff' />
+					<link rel='manifest' href='/favicons/site.webmanifest' />
+				</Head>
+
 				<body>
 					<Main />
 					<NextScript />
