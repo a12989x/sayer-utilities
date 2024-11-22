687b6c1f7815ccd3b13e0a99c1442a01f08fbe8e
Alexis Guzman <codingcodax@gmail.com>
2024-11-22 09:21:08 -0600

    feat(routes): render tools with card components

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,39 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
+import { Link } from '@remix-run/react';
+
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '~/components/ui/card';
+import { tools } from '~/config/tools';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Sayer" },
-    { name: "description", content: "Herramientas para Sayer - La Via" },
+    { title: 'Sayer' },
+    { name: 'description', content: 'Herramientas para Sayer - La Via' },
   ];
 };
 
 const Home = () => {
   return (
-    <div>
-      <p>home page</p>
+    <div className='p-4'>
+      <div className='flex flex-col gap-4'>
+        {tools.map((tool) => (
+          <Link key={tool.title} to={tool.url}>
+            <Card>
+              <CardHeader>
+                <div className='mb-2 w-fit rounded-md border p-2'>
+                  <tool.icon className='size-4' />
+                </div>
+                <CardTitle>{tool.title}</CardTitle>
+                <CardDescription>{tool.description}</CardDescription>
+              </CardHeader>
+            </Card>
+          </Link>
+        ))}
+      </div>
     </div>
   );
 };
