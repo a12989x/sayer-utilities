28abee70bfd217454d7eaed158b44fa1268b36b4
Alexis Guzman <codingcodax@gmail.com>
2024-11-23 09:03:48 -0600

    feat(config): create sizes array

---
+++ app/config/sizes.ts
@@ -0,0 +1,14 @@
+export const sizes = [
+  {
+    value: 'Litro',
+    label: 'Litro',
+  },
+  {
+    value: 'Galón',
+    label: 'Galón',
+  },
+  {
+    value: 'Cubeta',
+    label: 'Cubeta',
+  },
+];
